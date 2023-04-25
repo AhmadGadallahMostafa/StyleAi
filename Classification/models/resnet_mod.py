cb25ea3a8989151a902bf8a58541779044e12c85
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2023-04-25 07:40:21 +0200

    custom resnet code

---
+++ Classification/models/resnet_mod.py
@@ -0,0 +1,56 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import pretrainedmodels
+from models.resnet50_torch import ResNet50, ResNetBlock50
+from torchvision import models
+from torchvision.models import ResNet50_Weights
+import torch
+from torchsummary import summary
+
+class MultiHeadResNet(nn.Module):
+    def __init__(self, pre_trained, requires_grad):
+        super(MultiHeadResNet, self).__init__()
+        if pre_trained == True:
+            self.model = ResNet50(ResNetBlock50, 3, 1000, include_top = False)
+        else:
+            self.model = ResNet50(ResNetBlock50, 3, 1000, include_top = False, weights = None)
+        if requires_grad == True:
+            for param in self.model.parameters():
+                param.requires_grad = True
+            print('Training intermediate layer parameters...')
+        elif requires_grad == False:
+            for param in self.model.parameters():
+                param.requires_grad = False
+            print('Freezing intermediate layer parameters...')
+        
+        # change the final layers according to the number of categories
+        self.l0 = nn.Linear(2048, 5) # for gender
+        self.l1 = nn.Linear(2048, 11) # for subCategory
+        self.l2 = nn.Linear(2048, 46) # for articleType
+        self.l3 = nn.Linear(2048, 46) # for baseColour
+        self.l4 = nn.Linear(2048, 7) # for usage
+
+    def forward(self, x):
+        # get the batch size only, ignore (c, h, w)
+        batch, _, _, _ = x.shape
+        # get model output before the final layer
+        x = self.model(x)
+        x = F.adaptive_avg_pool2d(x, 1).reshape(batch, -1)
+        l0 = self.l0(x)
+        l1 = self.l1(x)
+        l2 = self.l2(x)
+        l3 = self.l3(x)
+        l4 = self.l4(x)
+        return l0, l1, l2, l3, l4
+
+# model = MultiHeadResNet(pre_trained = True, requires_grad = False)
+# model.to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
+# # compare with torchvision.models.resnet50
+# resnet50_torch = models.resnet50(weights = ResNet50_Weights.IMAGENET1K_V2)
+# resnet50_torch.to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
+# # print model summary
+# summary(model, (3, 224, 224))
+# summary(resnet50_torch, (3, 224, 224))
+# # print weights of the first layer
+# print(model.model.conv1.weight[0][0])
+# print(resnet50_torch.conv1.weight[0][0])
