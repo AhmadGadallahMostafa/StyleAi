7f70903fd9b3c6cff640270b72aff63048e46de2
Ahmad Gadallah <ahmadg@silverkeytech.com>
2023-05-02 02:33:53 +0300

    preProcessing openpose

--- Try-On/PreProcessing.py
+++ Try-On/PreProcessing.py
@@ -13,18 +13,22 @@ import cv2
 import numpy as np 
 from PIL import ImageOps
 from carvekit.api.high import HiInterface
-import torch
+import subprocess
 
 
 
     
 class PreProcessing:
+    def __init__(self, input_path_image = "Try-On/InputImages", input_path_cloth = "Try-On/InputClothesImages"):
+        self.input_path_image = input_path_image
+        self.input_path_cloth = input_path_cloth
+
 
     def get_densepose(self):
         # DensePose parameters
         apply_net_path_densepose = "Try-On/DensePose/apply_net.py"
         config_path_densepose = "Try-On/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml"
-        images_path_densepose = "Try-On/InputImages"
+        images_path_densepose = self.input_path_image
         model_path_densepose = "https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_50_FPN_s1x/165712039/model_final_162be9.pkl"
         # This will save the images with the same name as the input image at PreproceessedImages folder in densepose folder 
         command = "python " + apply_net_path_densepose + " show " + config_path_densepose + " " + model_path_densepose + " " + images_path_densepose + " dp_segm -v"
@@ -53,7 +57,7 @@ class PreProcessing:
 
         for filename in os.listdir("Try-On/InputClothesImages"):
             if filename.endswith(".jpg") or filename.endswith(".png"):
-                image = PIL.Image.open("Try-On/InputClothesImages/" + filename)
+                image = PIL.Image.open(self.input_path_cloth + '/' + filename)
                 # remove the extension from the filename
                 filename = filename.split(".")[0]
                 # this just removes the background from the image
@@ -67,9 +71,21 @@ class PreProcessing:
             else:
                 continue
 
+    def get_openpose(self):
+        # switch directory to openpose
+        os.chdir("Try-On/openPose")
+        exe_path = "bin/OpenPoseDemo.exe"
+        input_path =  "../../" + self.input_path_image
+        output_path = "../../" + "Try-On\PreprocessedImages\OpenPose"
+        output_json_path =  "../../" + "Try-On\PreprocessedImages\OpenPosejson"
+        command = exe_path + " --image_dir " + input_path + " --disable_blending --write_json " + output_json_path + " --display 0 -write_images " + output_path + " --num_gpu 1 --hand --num_gpu_start 0"
+        subprocess.run([exe_path, "--image_dir", input_path, "--disable_blending", "--write_json", output_json_path, "--display", "0", "--write_images", output_path, "--num_gpu", "1", "--hand", "--num_gpu_start", "0"])
+
+
     def run(self):
         self.get_densepose()
         self.get_mask()
+        self.get_openpose()
         
 
 def main():
